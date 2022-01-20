10cd066ab3bea3cb25ad4d2de194605c18dc7de4
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-19 20:59:06 -0600

    adding images and external data file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './styles/styles.css'
+import data from './data'
+import Entry from './components/Entry'
+import Navbar from './components/Navbar'
 
 function App() {
+  const entries = data.map(entry => <Entry key="entry.title" {...entry} />)
+
   return (
     <div className="App">
-     <h1> My travel journal </h1>
+      <Navbar />
+      <main className='journal'>
+        {entries}
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default [{
+        title: "Island of Janitzio",
+        location: "Michoacan, México",
+        googleMapsUrl: "https://goo.gl/maps/uZbHQ4Sb1SB6jcuZ9",
+        startDate: "2 Jul, 2018",
+        endDate: "3 Jul, 2018",
+        description: `Isla de Janitzio is the main island of Lake Pátzcuaro in the state of Michoacán, Mexico. The town of Janitzio, which means "where it rains", is located atop the hill. Janitzio can only be reached by boats which run regularly back and forth from about 7:30 am to 6 pm, accessible from Pátzcuaro's pier.`,
+        imageUrl: "janitzio.jpg"
+    },
+    {
+        title: "Hollywood Sign",
+        location: "Los Angeles, California, USA",
+        googleMapsUrl: "https://goo.gl/maps/sjCSms7hpyCRNaeJ8",
+        startDate: "29 Jan, 2018",
+        endDate: "30 Jan, 2018",
+        description: "The Hollywood Sign is an American landmark and cultural icon overlooking Hollywood, Los Angeles, California. It is situated on Mount Lee, in the Beachwood Canyon area of the Santa Monica Mountains.",
+        imageUrl: "los_angeles.jpg"
+    },
+    {
+        title: "St. Peter's Basilica",
+        location: "Vatican City, Italy",
+        googleMapsUrl: "https://goo.gl/maps/X8qPvE2oWBA99wc96",
+        startDate: "10 Dec, 2015",
+        endDate: "23 Dec, 2015",
+        description: "St. Peter's is a church built in the Renaissance style located in the Vatican City west of the River Tiber and near the Janiculum Hill and Hadrian's Mausoleum.",
+        imageUrl: "rome_pietro.jpg"
+    },
+    {
+        title: "Tequila",
+        location: "Jalisco, México",
+        googleMapsUrl: "https://goo.gl/maps/psmVLSWdiU6Gjjc6A",
+        startDate: "30 Jun, 2018",
+        endDate: "1 Jul, 2018",
+        description: "Tequila is a town in Mexico’s western state of Jalisco. It’s known for its production of the famous spirit. Near the foot of the Tequila Volcano, the outskirts of town are dotted with fields of blue agave, the liquor’s main ingredient.",
+        imageUrl: "tequila.jpg"
+    },
+    {
+        title: "San Juan del Sur",
+        location: "Nicaragua",
+        googleMapsUrl: "https://goo.gl/maps/aYRPMvjgCNo21KTx5",
+        startDate: "10 Dec, 2018",
+        endDate: "12 Dec, 2018",
+        description: "San Juan del Sur is a town on Nicaragua’s southwest coast. It’s known for a string of nearby Pacific beaches, such as Maderas Beach, with its strong-breaking waves, and the quieter Marsella Beach.",
+        imageUrl: "san_juan_del_sur.jpg"
+    },
+]
